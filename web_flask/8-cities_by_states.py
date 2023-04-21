2e98a012dc607cf6a00c114eaf24480133de3ced
sambilokonga <abebawbegna@gmail.com>
2023-04-21 13:03:30 -0700

    task 8 update

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,24 +1,31 @@
 #!/usr/bin/python3
+"""starts a Flask web application
 """
-starts a Flask web applicationstart Flask application"""
-
 from flask import Flask, render_template
-from models import *
 from models import storage
+from models.state import State
+
+
 app = Flask(__name__)
-
-
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    """display the states and cities listed in alphabetical order"""
-    states = storage.all("State").values()
-    return render_template('8-cities_by_states.html', states=states)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def close_db(exc):
+    """closes
+    """
     storage.close()
 
+
+@app.route('/cities_by_states')
+def cities_by_states():
+    """display a HTML page
+    """
+    states = storage.all(State).values()
+    return render_template("8-cities_by_states.html", states=states)
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+
